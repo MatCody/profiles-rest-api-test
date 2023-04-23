cf7d18c26dd5eebc7715e7ee230a00e616d6952e
Mateus Sousa <mateuss4570@gmail.com>
2023-04-23 01:35:28 -0300

    fix one

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$-3*m#w9nmk7nl96_--p6a@7lgmaxax&&-8tvmpmt33zn70fio'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
