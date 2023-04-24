709008fa3b9111795bf7117193860b57592d2c40
Mateus Sousa <mateuss4570@gmail.com>
2023-04-24 20:10:39 -0300

    Added POST, PUT, PATCH and DELETE methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of API View features"""
@@ -15,3 +18,31 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!','an_apiview': an_apiview})
+
+    def post(self, request):
+        '''Create a hello message with our name'''
+        serializer = self.serializer_class(data=request.data)
+    
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name} !'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    # THE DJANGO REST FRAMEWORK serializers PROVIDES THE FUNCTIONALITY TO VALIDATE
+    # THE INPUT, SO THAT'S INSURING THAT THE INPUT IS VALID AS PER SPECIFICATION OF OURS
+    # SERIALIZERS FIELD, OUR CASE -> the name no longer than 10 characters 
+
+    '''the pk take the ID of the object that we want to update on our request'''
+    def put(self, request, pk=None):
+        '''Handle updating an object'''
+        return Response({'method':'PUT'})
+    
+    def patch(self, request, pk=None):
+        '''if we want to update but maintain the past information'''
+        '''Handle a partial update of an object'''
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        '''Delete an object'''
+        return Response({'method':'DELETE'})
